ebec4ebc5198ef3ddde82ec42dd9c48d6325351b
TharukaSamarakoon <www.tharuka.s2@gmail.com>
2026-09-26 16:35:55 +0530

    remove

--- Controller/Buyer/ForgotPasswordController.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once __DIR__ . '/../../config/app.php';
-redirect('index.php?page=forgot_password');
--- Controller/Buyer/RegistrationController.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once __DIR__ . '/../../config/app.php';
-redirect('index.php?page=signup_buyer');
